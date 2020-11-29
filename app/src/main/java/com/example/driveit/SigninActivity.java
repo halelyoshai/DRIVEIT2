6aa4da868fbb501571119d95b374ed5d57e48761
halelyoshai <halelyoshai123@gmail.com>
2020-11-29 23:25:57 +0200

    29/11

--- app/src/main/java/com/example/driveit/SigninActivity.java
+++ app/src/main/java/com/example/driveit/SigninActivity.java
@@ -1,4 +1,43 @@
 package com.example.driveit;
 
-public class SigninActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+    private View v;
+    private TextView driveit;
+    private Button username;
+    private Button password;
+    private Button signin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        driveit = findViewById(R.id.txtdriveit);
+        username = findViewById(R.id.btnhome);
+        username.setOnClickListener(this);
+        password = findViewById(R.id.btnprogress);
+        password.setOnClickListener(this);
+        signin = findViewById(R.id.btncontrol);
+        signin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == signin) {
+            Intent intent = new Intent( this,
+                    StudentprofileActivity.class);
+            startActivity(intent);
+        }
+
+    }
 }
--- app/src/main/java/com/example/driveit/StudentsratingActivity_S.java
+++ app/src/main/java/com/example/driveit/StudentsratingActivity_S.java
@@ -2,13 +2,50 @@ package com.example.driveit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class StudentsratingActivity_S extends AppCompatActivity {
+public class StudentsratingActivity_S extends AppCompatActivity implements View.OnClickListener {
+    private View v;
+    private TextView rating;
+    private Button home;
+    private Button progress;
+    private Button control;
+    private Button parking;
+    private Button caution;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tudentsrating1);
+        setContentView(R.layout.activity_studentsrating);
+
+        rating = findViewById(R.id.txtstudentsrating);
+        home = findViewById(R.id.btnhome);
+        home.setOnClickListener(this);
+        progress = findViewById(R.id.btnprogress);
+        progress.setOnClickListener(this);
+        control = findViewById(R.id.btncontrol);
+        control.setOnClickListener(this);
+        parking = findViewById(R.id.btnparking);
+        parking.setOnClickListener(this);
+        caution = findViewById(R.id.btncaution);
+        caution.setOnClickListener (this);
+        save = findViewById(R.id.save);
+        save.setOnClickListener (this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == home) {
+            Intent intent = new Intent( this, StudentprofileActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
