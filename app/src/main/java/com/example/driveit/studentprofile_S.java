c6310658be77d20820ece033e2d5d89fc66fe035
halelyoshai <halelyoshai123@gmail.com>
2021-01-06 19:08:01 +0200

    6\1

---
+++ app/src/main/java/com/example/driveit/studentprofile_S.java
@@ -0,0 +1,14 @@
+package com.example.driveit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class studentprofile_S extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentprofile__s);
+    }
+}
