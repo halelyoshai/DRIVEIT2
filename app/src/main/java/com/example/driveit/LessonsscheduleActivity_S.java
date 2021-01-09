642a0e46cf56cde6527b4302b5aec2d47e9fa76a
halelyoshai <halelyoshai123@gmail.com>
2021-01-09 23:10:59 +0200

    9.1

--- app/src/main/java/com/example/driveit/LessonsscheduleActivity_S.java
+++ app/src/main/java/com/example/driveit/LessonsscheduleActivity_S.java
@@ -9,6 +9,6 @@ public class LessonsscheduleActivity_S extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lessinsschedule);
+        setContentView(R.layout.activity_lessonsschedule);
     }
 }
