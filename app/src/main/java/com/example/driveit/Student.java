cb5c22d6c9a3144e533cb25ad1addeab2097e257
halelyoshai <halelyoshai123@gmail.com>
2021-02-07 22:44:23 +0200

    7\2

--- app/src/main/java/com/example/driveit/Student.java
+++ app/src/main/java/com/example/driveit/Student.java
@@ -3,9 +3,48 @@ package com.example.driveit;
 public class Student extends Person
 {
 
+    private int testpnimi;
+    private int numlessons;
+    private int numtests;
+    private Teacher t;
 
-
-    public Student(String name, String id, String phonenum, String mail, String studyarea, String school, String city, String lisensetype, String username, String password) {
+    public Student(String name, String id, String phonenum, String mail, String studyarea, String school, String city, String lisensetype, String username, String password, int testpnimi, int numlessons, int numtests, Teacher t) {
         super(name, id, phonenum, mail, studyarea, school, city, lisensetype, username, password);
+        this.testpnimi = testpnimi;
+        this.numlessons = numlessons;
+        this.numtests = numtests;
+        this.t = t;
+    }
+
+    public int getTestpnimi() {
+        return testpnimi;
+    }
+
+    public void setTestpnimi(int testpnimi) {
+        this.testpnimi = testpnimi;
+    }
+
+    public int getNumlessons() {
+        return numlessons;
+    }
+
+    public void setNumlessons(int numlessons) {
+        this.numlessons = numlessons;
+    }
+
+    public int getNumtests() {
+        return numtests;
+    }
+
+    public void setNumtests(int numtests) {
+        this.numtests = numtests;
+    }
+
+    public Teacher getT() {
+        return t;
+    }
+
+    public void setT(Teacher t) {
+        this.t = t;
     }
 }
--- app/src/main/java/com/example/driveit/Teacher.java
+++ app/src/main/java/com/example/driveit/Teacher.java
@@ -5,10 +5,58 @@ import java.util.ArrayList;
 public class Teacher extends Person
 {
     private ArrayList<Student> students;
+    private String lessonlength;
+    private String Swaitingtest;
+    private String freelessons;
+    private String status;
 
 
-    public Teacher(String name, String id, String phonenum, String mail, String studyarea, String school, String city, String lisensetype, String username, String password) {
+    public Teacher(String name, String id, String phonenum, String mail, String studyarea, String school, String city, String lisensetype, String username, String password, ArrayList<Student> students, String lessonlength, String swaitingtest, String freelessons, String status) {
         super(name, id, phonenum, mail, studyarea, school, city, lisensetype, username, password);
         students=new ArrayList<Student>();
+        this.lessonlength = lessonlength;
+        this.Swaitingtest = swaitingtest;
+        this.freelessons = freelessons;
+        this.status = status;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public String getLessonlength() {
+        return lessonlength;
+    }
+
+    public void setLessonlength(String lessonlength) {
+        this.lessonlength = lessonlength;
+    }
+
+    public String getSwaitingtest() {
+        return Swaitingtest;
+    }
+
+    public void setSwaitingtest(String swaitingtest) {
+        Swaitingtest = swaitingtest;
+    }
+
+    public String getFreelessons() {
+        return freelessons;
+    }
+
+    public void setFreelessons(String freelessons) {
+        this.freelessons = freelessons;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
