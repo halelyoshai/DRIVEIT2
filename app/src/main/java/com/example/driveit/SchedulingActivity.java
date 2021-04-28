51b9e776152d0cf080bf2cb98d191087c9c871df
netalalush <netalalush5@gmail.com>
2021-04-28 15:00:23 +0300

    20/4/21

--- app/src/main/java/com/example/driveit/SchedulingActivity.java
+++ app/src/main/java/com/example/driveit/SchedulingActivity.java
@@ -3,12 +3,21 @@ package com.example.driveit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class SchedulingActivity extends AppCompatActivity {
+    CalendarView simpleCalendarView = (CalendarView) findViewById(R.id.CalendarView);
+    long selectedDate = simpleCalendarView.getDate();
+    CalendarView simpleCalendarView = (CalendarView) findViewById(R.id.CalendarView);
+simpleCalendarView.setFirstDayOfWeek(1);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheduling);
+
     }
+
 }
+
